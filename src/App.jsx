2af4d8a9f27b90627f50c361388cc40c3338aac7
phuongez <duyphuong.hvnh@gmail.com>
2025-03-09 22:33:22 +0700

    Display all tasks

--- src/App.jsx
+++ src/App.jsx
@@ -7,15 +7,36 @@ import AllToDos from './components/AllTodos'
 
 function App() {
   const [displayAll, setDisplayAll] = useState(true)
+  const [displayActive, setDisplayActive] = useState(false)
+  const [displayCompleted, setDisplayCompleted] = useState(false)
 
   return (
     <div className="app-view">
       <header>#Phuong'sToDo</header>
       <main>
         <div className="nav">
-          <button onClick={()=>setDisplayAll(true)} id="all-todos">All</button>
-          <button id="active-todos">Active</button>
-          <button id="completed-todos">Completed</button>
+          <button 
+            onClick={()=> {
+              setDisplayAll(true)
+              setDisplayActive(false)
+              setDisplayCompleted(false)
+            }} 
+            id="all-todos"
+          >All</button>
+          <button
+            onClick={()=> {
+              setDisplayAll(false)
+              setDisplayActive(true)
+              setDisplayCompleted(false)
+            }}  
+            id="active-todos">Active</button>
+          <button
+            onClick={()=> {
+              setDisplayAll(false)
+              setDisplayActive(false)
+              setDisplayCompleted(true)
+            }}  
+            id="completed-todos">Completed</button>
         </div>
         <Divider />
         {displayAll && <AllToDos />}
